a65883792d80d7ec9be2eb506cfb2ae8937ee48a
shawon-lab-debug <roknuzzamanshawon2022@gmail.com>
2026-09-16 15:11:02 +0600

    check remove update

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -49,7 +49,7 @@ function Footer() {
         </div>
 
         <div className="footer-bottom">
-          <p>© 2026 Dev Stack. All rights reserved.</p>
+          <p>© 2026 Dev Stack. All rights reserved by Dev Stack</p>
 
           <div>
             <a href="#">Privacy</a>
